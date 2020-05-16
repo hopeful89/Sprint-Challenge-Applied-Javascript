8e72d412bf9dd5fb75c893334e5b5e9b512f9361
Your Name <you@example.com>
2020-05-16 14:59:37 -0400

    worked on carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -11,9 +11,94 @@
   <div class="carousel">
     <div class="left-button"> < </div>
     <img src="./assets/carousel/mountains.jpeg" />
-    <img src="./assets/carousel/computer.jpeg" />
+    <img src"="./assets/carousel/computer.jpeg />
     <img src="./assets/carousel/trees.jpeg" />
     <img src="./assets/carousel/turntable.jpeg" />
     <div class="right-button"> > </div>
   </div>
 */
+const imgArrayRef = ["./assets/carousel/mountains.jpeg", "./assets/carousel/computer.jpeg", "./assets/carousel/trees.jpeg", "./assets/carousel/turntable.jpeg"]
+function stretchCarosel(imgArray) {
+  carouselAppend = document.querySelector('.carousel-container')
+  //html markup creation
+  let carouselContainer = document.createElement('div');
+  let leftButton = document.createElement('div');
+  let rightButton = document.createElement('div');
+
+  //Parent Child
+  carouselContainer.append(leftButton)
+  imgArray.forEach(el => {
+    let image = document.createElement('img');
+    image.src = el;
+    carouselContainer.append(image)
+  })
+  carouselContainer.append(rightButton)
+
+  //Class list
+  carouselContainer.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  //add content
+  leftButton.textContent = ' < ';
+  rightButton.textContent = ' > ';
+  carouselAppend.append(carouselContainer)
+  return carouselContainer
+}
+stretchCarosel(imgArrayRef)
+
+function buttons(){
+  let newone = document.querySelectorAll('.carousel-container img');
+  newone[0].style.display = 'inline-block';
+  let rightButton = document.querySelector('.right-button');
+  let leftButton = document.querySelector('.left-button');
+  let rightInterval;
+  let i = 0;
+
+  setInterval(rightButton.addEventListener('click', () => {    
+    if(i <= newone.length){
+      newone[i].style.display = 'none';
+      i++
+        if(i <= newone.length - 1 && i != 0){
+          newone[i].style.display = 'inline-block';
+        }
+        if(i === newone.length){
+          newone[0].style.display = 'inline-block'
+          i = 0;
+        }
+      }
+    }), 1000)
+
+
+    // leftButton.addEventListener('click', () => {    
+    //   if(i <= newone.length && i < 0){
+    //     newone[i].style.display = 'none';
+    //     i--
+    //       if(i <= newone.length - 1 && i != 0){
+    //         newone[i].style.display = 'inline-block';
+    //       }
+    //       if(i === newone.length){
+    //         newone[0].style.display = 'inline-block'
+    //         i = 0;
+    //       }
+    //     }
+    //   })
+}
+buttons()
+  //   if(i + 1 < newone.length){
+  //     newone[i+1].style.display = 'inline-block';
+  //     i++
+  //     if(i === newone.length){
+  //       newone[newone.length - 1].style.display = 'inline-block'
+  //     }
+  //   }
+  // }else{
+  //   i = 0
+  // }
+// })
+
+
+
+
+
+
